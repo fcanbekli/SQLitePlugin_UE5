792966a2101a3e2c3a3d0b7ec5a96864668c4ae6
StrahdVonZarovich <fatih.canbekli@outlook.com>
2021-06-22 22:45:19 +0300

    Initialize project

---
+++ Source/SQLLitePlugin_UE5/SQLLitePlugin_UE5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SQLLitePlugin_UE5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SQLLitePlugin_UE5, "SQLLitePlugin_UE5" );
---
+++ Source/SQLLitePlugin_UE5/SQLLitePlugin_UE5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SQLLitePlugin_UE5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQLLITEPLUGIN_UE5_API ASQLLitePlugin_UE5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
