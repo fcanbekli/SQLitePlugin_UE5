675ee10b7e3036acf67223ba2316804ec5a9657b
StrahdVonZarovich <fatih.canbekli@outlook.com>
2021-08-15 14:57:54 +0300

    Remove unnecessary

--- Plugins/UE_SQLLite/Source/UE_SQLLite/Public/SQL_Toolset.h
+++ Plugins/UE_SQLLite/Source/UE_SQLLite/Public/SQL_Toolset.h
@@ -18,17 +18,17 @@ class UE_SQLLITE_API USQL_Toolset : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 public:
 
-	UFUNCTION(BlueprintCallable, Category = Almyra)
+	UFUNCTION(BlueprintCallable, Category = SQLite)
 		static void TestBlueFunction();
 
-	UFUNCTION(BlueprintCallable, Category = Almyra)
+	UFUNCTION(BlueprintCallable, Category = SQLite)
 		static void ExecuteSQLCommand(FString dbPath, FString sqlCommand);
 
 
-	UFUNCTION(BlueprintCallable, Category = Almyra)
+	UFUNCTION(BlueprintCallable, Category = SQLite)
 		static int SQL_GetInt(FString dbPath, FString sqlCommand);
 
-	UFUNCTION(BlueprintCallable, Category = Almyra)
+	UFUNCTION(BlueprintCallable, Category = SQLite)
 		static FString SQL_GetString(FString dbPath, FString sqlCommand);
 
 	
